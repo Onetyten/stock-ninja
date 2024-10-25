e5ffd7a2867156d2be410b2b28c7cb432178bc85
labae <onetyten@gmail.com>
2024-10-25 15:37:45 +0100

    Nav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,11 @@ function App() {
   
   return (
     <MyProvider>
-      <main className='grid bg-my-Dark-teal w-full min-h-screen bg-gradient-to-tr from-my-Charcoal to-my-mossy-green grid-cols-1 md:grid-cols-3 auto-rows-[70px] auto-cols-fr gap-6 font-quicksand text-white '>
-        <div>
+      <main className='w-screen flex flex-col md:flex-row'>
+        <div className='w-screen h-16 md:w-2/6 md:min-h-screen '>
           <Nav/>
+        </div>
+        <div className='grid bg-my-Dark-teal w-full min-h-screen grid-cols-1 md:grid-cols-3 auto-rows-[70px] auto-cols-fr gap-6 font-quicksand text-white '>
         </div>
       </main>
     </MyProvider>
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Logo from './Logo.png'
 
 export default function () {
   return (
-    <div className='bg-my-Charcoal w-full h-full rounded-b-xl border-b-4 border-my-Dark-teal'>
+    <div className='bg-my-Charcoal w-full h-full rounded-b-xl border-2 border-t-0 border-my-Dark-teal'>
+        <div className='flex justify-between px-2 w-full h-full items-center'>
+            <div>
+                <img src={Logo} alt="" className='w-10 h-10 rounded-full' />
+            </div>
+            <div className='flex gap-4'>
+                <div className='flex flex-col justify-center items-center'>
+                    <i className="fa-solid fa-house text-my-green-light text-lg"></i>
+                    <p className='text-xs text-my-green-light'>Home</p>
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                    <i className="fa-solid fa-chart-line text-stone-600 text-lg"></i>
+                    <p className='text-xs text-stone-600'>Analytics</p>
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                    <i className="fa-solid fa-wallet text-stone-600 text-lg"></i>
+                    <p className='text-xs text-stone-600'>Wallet</p>
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                    <i className="fa-regular fa-newspaper text-stone-600 text-lg"></i>
+                    <p className='text-xs text-stone-600'>Discover</p>
+                </div>
+
+            </div>
+        </div>
 
     </div>
   )
